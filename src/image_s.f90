2ef9139588ed88fba5b7ee5df47124db218a109a
Damian Rouson <damian@sourceryinstitute.org>
2021-03-16 14:40:36 -0700

    feat: add task_t & compute_t & task test

--- src/image_s.f90
+++ src/image_s.f90
@@ -1,51 +1,36 @@
 submodule(image_m) image_s
-  use iso_fortran_env, only : event_type
   use assertions_interface, only : assert
   implicit none
 
-  type(event_type), allocatable :: ready_for_next_task(:)[:]
-  type(event_type) task_assigned[*]
-
-  integer, parameter :: scheduler_image = 1
+  type(event_type), allocatable :: ready_for_next_task_(:)[:]
+  type(event_type) task_assigned_[*]
 
 contains
 
   module procedure set_up
-
     logical, save :: first_call = .true.
 
     call assert(first_call, "image_t%set_up: first_call")
 
     first_call = .false.
-
-    if (this_image() == scheduler_image) self%scheduler_ = .true.
-    allocate(ready_for_next_task(2:num_images())[*])
+    allocate(ready_for_next_task_(num_images())[*])
 
   end procedure
 
-  module procedure scheduler
-    I_am_scheduler = this_image() == scheduler_image
-  end procedure
-
-  module procedure scheduler_assigns_task
-
-    call assert(self%scheduler(), "image_t%scheduler_assigns_task: self%scheduler()")
-
-    event post(task_assigned[compute_image])
-
+  module procedure assign_task
+    event post(task_assigned_[compute_image])
   end procedure
 
   module procedure wait_do_task_notify_ready
+    call assert(allocated(ready_for_next_task_), "image_t%wait_do_task_notify_ready: allocated(self%ready_for_next_task_)")
 
-    call assert(.not. self%scheduler(), "image_t%wait_do_task_notify_ready: .not. self%scheduler()")
-    call assert(allocated(ready_for_next_task), "image_t%wait_do_task_notify_ready: allocated(ready_for_next_task)")
-
-    event wait(task_assigned)
-
+    event wait(task_assigned_)
     ! do task
+    event post(ready_for_next_task_(this_image())[self%scheduler_image_])
+  end procedure
 
-    event post(ready_for_next_task(this_image())[scheduler_image])
-
+  module procedure scheduler_image
+    image_number = self%scheduler_image_
   end procedure
 
 end submodule image_s
--- src/task_s.f90
+++ src/task_s.f90
@@ -4,12 +4,14 @@ submodule(task_m) task_s
 contains
 
   module procedure do_work
-    !! execute the a subroutine if the task defines one and report result
+    !! execute  a subroutine if the task defines one and report result;
+    !! otherwise, report that no work is being done
     if (present(verbose)) then
       if (this%identifier_ == no_work) then
         print *, "Image", this_image(), "does no work"
       else
         print *, "Image", this_image(), "does work on task", this%identifier_
+        ! Write code here to call the task's subroutine
       end if
     end if
   end procedure
