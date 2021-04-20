7ec27e2454273f391f0fd8a0616e4337824ce285
Damian Rouson <damian@sourceryinstitute.org>
2021-04-20 13:38:50 -0700

    remove unnecessary factory; update app/test

--- tests/application_test.f90
+++ tests/application_test.f90
@@ -1,22 +1,21 @@
-module application_factory_test
-   !! verify application_factory protocol for task assignment, completion, & readiness notification
+module application_test
+   !! verify application protocol for task assignment, completion, & readiness notification
    use vegetables, only: &
      result_t, test_item_t, & ! types
      describe, it, succeed ! functions
-   use application_factory_m, only : application_factory_t
    use application_m, only : application_t
    implicit none
 
    private
-   public :: test_application_factory
+   public :: test_application
 
 contains
 
-  function test_application_factory() result(tests)
+  function test_application() result(tests)
     type(test_item_t) tests
 
     tests = describe( &
-     "application_factory class", &
+     "application class", &
      [it( &
        "constructs an application", &
        verify_application_construction) &
@@ -26,13 +25,12 @@ contains
 
   function verify_application_construction() result(result_)
     type(application_t) application
-    type(application_factory_t) application_factory
     type(result_t) result_
 
-    application = application_factory%factory_method()
+    !application = application_t()
 
     result_ = succeed("application constructed")
 
   end function
 
-end module application_factory_test
+end module application_test
