31957b4ce15112aa9f79344bd5ad7662fcccf36a
Damian Rouson <damian@sourceryinstitute.org>
2021-08-19 08:43:47 -0700

    doc(build_feats): fix dependency tree diagram

--- example/build-feats/application_generator_m.f90
+++ example/build-feats/application_generator_m.f90
@@ -14,27 +14,40 @@ contains
         ! To Do
         ! -----
         !
-        ! Insert code to construct module_dependencies to represent 
+        ! Insert code to define module_dependencies to represent 
         ! the following dependency tree:
         !
         ! build_feats (main)
-        !     |-- application_generator_m
-        !     |-- image_m
-        !     |   |-- data-location-map_m
-        !     |   |-- application_m
-        !     |       |-- dag_interface
-        !     |       |-- task_item_m
-        !     |           |-- data_location_map_m
-        !     |           |-- payload_m
-        !     |           |-- task_m
-        !     |               |-- data_location_map_m
-        !     |               |-- payload_m
-        !
-        !  application_s
-        !     |-- assertions_interface (external)
-        !
-        !  mailbox_m
-        !     |-- payload_m
+        ! |-- application_generator_m
+        ! |-- image_m
+        !     |-- data_location_map_m
+        !     |-- application_m
+        !         |-- dag_interface
+        !         |-- task_item_m
+        !             |-- data_location_map_m
+        !             |-- payload_m
+        !             |-- task_m
+        !                 |-- data_location_map_m
+        !                 |-- payload_m
+        !         
+        ! mailbox_m
+        ! |-- payload_m
+        ! 
+        ! application_s
+        ! |-- application_m
+        ! |-- assertions_interface (external)
+        ! 
+        ! payload_s
+        ! |-- payload_m
+        ! 
+        ! data_location_map_s
+        ! |-- data_location_map_m
+        ! 
+        ! image_s
+        ! |-- image_m
+        ! 
+        ! task_item_s
+        ! |-- task_item_m
         
         application = application_t(module_dependencies, [ task_item_t :: ])
     end function
