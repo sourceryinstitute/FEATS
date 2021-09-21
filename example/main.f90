412d6ab32ea357c7555b700f96129c66c996b227
Damian Rouson <damian@sourceryinstitute.org>
2021-09-21 14:02:26 -0700

    refac(example): rm unneeded variable/use statement

--- example/main.f90
+++ example/main.f90
@@ -1,15 +1,12 @@
 program main
   !! Framework for Extensible Asynchronous Task Scheduling
-  use application_m, only: application_t
   use application_generator_m, only : generate_application
   use image_m, only: image_t
   use task_payload_map_m, only: task_payload_map_t
   implicit none
 
-  type(application_t) :: application
   type(image_t) :: image
   type(task_payload_map_t) :: results
 
-  application = generate_application()
-  results = image%run(application)
+  results = image%run(generate_application())
 end program
