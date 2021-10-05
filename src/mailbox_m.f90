51266534d10e49fe6ef7d69b2cf22e08132c5719
everythingfunctional <everythingfunctional@protonmail.com>
2021-10-05 15:42:32 +0000

    Deploying to gh-pages from @ sourceryinstitute/FEATS@3dd952df8e5c1acfa2e695128f818c8bd1c23d92 🚀

--- src/mailbox_m.f90
+++ src/mailbox_m.f90
@@ -2,11 +2,15 @@ module mailbox_m
     use payload_m, only: payload_t
     implicit none
     private
-    public :: mailbox, mailbox_entry_can_be_freed
+    public :: payload_list_t, mailbox, mailbox_entry_can_be_freed
 
-    type(payload_t), allocatable :: mailbox(:)[:]
+    type :: payload_list_t
+        type(payload_t), allocatable :: payloads(:)
+    end type
+
+    type(payload_list_t), allocatable :: mailbox[:]
     !! storage for communicating inputs/outputs between tasks
 
     logical, allocatable :: mailbox_entry_can_be_freed(:)[:]
-    !! used by the scheduler image to tell the worker images when they can release old data. 
+    !! used by the scheduler image to tell the worker images when they can release old data.
 end module
--- src/task_payload_map_s.f90
+++ src/task_payload_map_s.f90
@@ -5,12 +5,12 @@ submodule(task_payload_map_m) task_payload_map_s
 contains
     module procedure get_task_ids
         ids = self%task_ids_
-    end procedure 
+    end procedure
 
     module procedure constructor
         if (size(task_ids) /= size(image_nums)) then
             error stop "task_payload_map_t%constructor: mismatched argument lengths"
-        end if 
+        end if
         new_map%task_ids_   = task_ids
         new_map%image_nums_ = image_nums
     end procedure
@@ -26,7 +26,7 @@ contains
                 end if
             else
                 associate( img => self%image_nums_(idx) )
-                    pl = mailbox(taskid)[img]%payload_ ! work around gfortran bug: direcly access payload_
+                    pl = mailbox[img]%payloads(taskid)%payload_ ! work around gfortran bug: direcly access payload_
                 end associate
             end if
         end associate
@@ -39,7 +39,7 @@ contains
             call self%get_raw_payload(taskid, raw, key_error)
         else
             call self%get_raw_payload(taskid, raw)
-        end if 
+        end if
         allocate(character(len=size(raw)) :: pl)
         do concurrent (i = 1 : size(raw))
             pl(i:i) = raw(i)
