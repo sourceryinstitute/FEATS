2bf9215aa42170ba5e4fc12dd6043262620b3834
Brad Richardson <everythingfunctional@protonmail.com>
2023-01-19 13:50:06 -0600

    fix: storing string payload

--- src/payload_s.f90
+++ src/payload_s.f90
@@ -15,27 +15,16 @@ contains
     end procedure
 
     module procedure from_string
-        integer :: string_length, length_as_payload, end_of_transmittable_string
-
-        string_length = len(payload)
-        length_as_payload = storage_size(char(1))/storage_size(1)*string_length + 1
-        if (length_as_payload > MAX_PAYLOAD_SIZE) then
-        block
-            integer, parameter :: end_of_transmittable_string = storage_size(1)/storage_size(char(1))*(MAX_PAYLOAD_SIZE-1)
-
-            new_payload%payload_size = MAX_PAYLOAD_SIZE
-            new_payload%payload_ =  &
-                    [ end_of_transmittable_string &
-                    , transfer(payload(1:end_of_transmittable_string), new_payload%payload_(2:)) &
-                    ]
-        end block
-        else
-            new_payload%payload_size = length_as_payload
-            new_payload%payload_ = &
-                    [ string_length &
-                    , transfer(payload, new_payload%payload_(1:length_as_payload)) &
-                    ]
-        end if
+        new_payload%payload_(1) = len(payload)
+        associate(string_as_integers => transfer(payload, new_payload%payload_))
+            if (size(string_as_integers) > MAX_PAYLOAD_SIZE-1) then
+                new_payload%payload_size = MAX_PAYLOAD_SIZE
+                new_payload%payload_(2:MAX_PAYLOAD_SIZE) = string_as_integers(1:MAX_PAYLOAD_SIZE-1)
+            else
+                new_payload%payload_size = size(string_as_integers) + 1
+                new_payload%payload_(2:new_payload%payload_size) = string_as_integers
+            end if
+        end associate
     end procedure
 
     module procedure empty_payload
@@ -48,7 +37,7 @@ contains
     module procedure string_payload
         if (self%payload_size > 0) then
             allocate(character(len=self%payload_(1)) :: string_payload)
-            if (self%payload_(1)> 0) &
+            if (self%payload_(1) > 0) &
                     string_payload = transfer( &
                             self%payload_(2:self%payload_size), string_payload)
         else
