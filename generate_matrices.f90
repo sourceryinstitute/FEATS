557c5b82760c0d733a6c152d6c70e273c4b4d8d9
Brad Richardson <everythingfunctional@protonmail.com>
2023-08-01 18:26:32 -0500

    use random numbers for matrices so they're solvable

--- generate_matrices.f90
+++ generate_matrices.f90
@@ -1,17 +1,17 @@
 program generate_matrices
     implicit none
 
-    integer, parameter :: max_size = 50
+    integer, parameter :: max_size = 10
     integer :: row, col, m_size, fu
     real, allocatable :: matrix(:, :)
     character(len=:), allocatable :: file_name
     character(len=2) :: size_string
 
+    call random_init(.true., .true.)
     do m_size = 1, max_size
         allocate(matrix(m_size, m_size))
-        do concurrent (row = 1:m_size, col = 1:m_size)
-            matrix(row, col) = (row-1)*m_size + col
-        end do
+        call random_number(matrix)
+        matrix = matrix*999 + 1 ! lets do numbers between 1 and 1000
         write(size_string, "(I0)") m_size
         file_name = "example/lu_decomp/" // trim(size_string) // "x" // trim(size_string) // ".dat"
         open(newunit=fu, file=file_name, status="replace")
