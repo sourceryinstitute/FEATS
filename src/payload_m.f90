1a24a390fb47c1cfe966c4ac85ae944032304584
Brad Richardson <everythingfunctional@protonmail.com>
2023-08-23 19:40:09 -0500

    feat: implement an algorithm that doesn't require a scheduler

--- src/payload_m.f90
+++ src/payload_m.f90
@@ -3,8 +3,6 @@ module payload_m
     private
     public :: payload_t, empty_payload
 
-    integer, parameter :: MAX_PAYLOAD_SIZE = 20002
-
     type :: payload_t
         !! A raw buffer to facilitate data transfer between  images
         !!
@@ -13,8 +11,7 @@ module payload_m
         !! * produce a string representation of the data, and then parse that string to recover the original data
         !! * use the `transfer` function to copy the raw bytes of the data
         private
-        integer, public :: payload_(MAX_PAYLOAD_SIZE)
-        integer :: payload_size = 0
+        integer, public, allocatable :: payload_(:)
     contains
         private
         procedure, public :: raw_payload
@@ -32,16 +29,7 @@ contains
         integer, intent(in) :: payload(:)
         type(payload_t) :: new_payload
 
-        integer :: incoming_payload_size
-
-        incoming_payload_size = size(payload)
-        if (incoming_payload_size > MAX_PAYLOAD_SIZE) then
-            new_payload%payload_size = MAX_PAYLOAD_SIZE
-            new_payload%payload_ = payload(1:MAX_PAYLOAD_SIZE)
-        else
-            new_payload%payload_size = incoming_payload_size
-            new_payload%payload_(1:incoming_payload_size) = payload
-        end if
+        allocate(new_payload%payload_, source = payload)
     end function
 
     pure function from_string(payload) result(new_payload)
@@ -49,21 +37,14 @@ contains
         character(len=*), intent(in) :: payload
         type(payload_t) :: new_payload
 
-        new_payload%payload_(1) = len(payload)
-        associate(string_as_integers => transfer(payload, new_payload%payload_))
-            if (size(string_as_integers) > MAX_PAYLOAD_SIZE-1) then
-                new_payload%payload_size = MAX_PAYLOAD_SIZE
-                new_payload%payload_(2:MAX_PAYLOAD_SIZE) = string_as_integers(1:MAX_PAYLOAD_SIZE-1)
-            else
-                new_payload%payload_size = size(string_as_integers) + 1
-                new_payload%payload_(2:new_payload%payload_size) = string_as_integers
-            end if
-        end associate
+        new_payload = payload_t([len(payload), transfer(payload, new_payload%payload_)])
     end function
 
     pure function empty_payload()
         implicit none
         type(payload_t) :: empty_payload
+
+        allocate(empty_payload%payload_(0))
     end function
 
     pure function raw_payload(self)
@@ -71,7 +52,7 @@ contains
         class(payload_t), intent(in) :: self
         integer, allocatable :: raw_payload(:)
 
-        raw_payload = self%payload_(1:self%payload_size)
+        raw_payload = self%payload_
     end function
 
     pure function string_payload(self)
@@ -79,11 +60,11 @@ contains
         class(payload_t), intent(in) :: self
         character(len=:), allocatable :: string_payload
 
-        if (self%payload_size > 0) then
+        if (size(self%payload_) > 0) then
             allocate(character(len=self%payload_(1)) :: string_payload)
             if (self%payload_(1) > 0) &
                     string_payload = transfer( &
-                            self%payload_(2:self%payload_size), string_payload)
+                            self%payload_(2:), string_payload)
         else
             allocate(character(len=0) :: string_payload)
         end if
